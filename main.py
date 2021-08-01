00dac8cd677ddaf09d4eae7f5fec03dfbde088ab
YasinRezvani <yasinrezvani2025@gmail.com>
2021-08-01 23:17:08 +0430

    define function and in function have loop while seconds == 0 and print (time complted)

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 ﻿import time
+
+def countdown(t):
+    while t:
+        min , sec = divmod(t , 60)
+        timer = "{:02d}:{:02d}".format(min,sec)
+        print(timer , end = "\r")
+        time.sleep(1)
+        t -= 1
+    print("Time Completed")    
+
+t = input("Enter The Time in secends: ")
+countdown(int(t))
+
+
+# Made By Yasin Rezvani
